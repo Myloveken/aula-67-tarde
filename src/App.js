b2e12659466e3ea114c7700798e3b05c4ea4d11a
Kenlove Chereste <cherestek@gmail.com>
2022-06-19 21:12:15 -0300

    Meu components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line no-unused-vars
+import UseStateClass from './components/UseStateClass';
+// eslint-disable-next-line no-unused-vars
+import UseStateFunction from './components/UseStateFunction';
+// eslint-disable-next-line no-unused-vars
+import UseStateLazy from './components/UseStateLazy';
+// eslint-disable-next-line no-unused-vars
+import UsePrevState from './components/UsePrevState';
+// eslint-disable-next-line no-unused-vars
+import UseStateObject from './components/UseStateObject';
+// eslint-disable-next-line no-unused-vars
+import UseEffectClass from './components/UseEffectClass';
+// eslint-disable-next-line no-unused-vars
+import UseEffectFunction from './components/UseEffectFunction';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" > {
+            /* <UsePrevState />
+                  <UseStateClass />
+                  <UseStateFunction />
+                  <UseStateLazy /> */
+        } { /* <UseStateObject /> */ } { /* <UseEffectClass /> */ } { /* <UseEffectFunction /> */ } <
+        TodoList / >
+        <
+        /div>
+    );
 }
 
 export default App;
