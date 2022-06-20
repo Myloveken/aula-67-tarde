f19e92118ca64608d0395668e8f9453b205cd5ce
Kenlove Chereste <cherestek@gmail.com>
2022-06-19 21:03:29 -0300

    Transforma-Tec-Aula67

---
+++ src/Components/UseEffectFunction.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+
+export default function UseEffectFunction() {
+
+
+    const [count, setCount] = useState(0)
+
+    useEffect(
+        () => {
+            document.title = `Cliques: ${count}`
+        }, [count]
+    )
+
+    return ( <
+        div >
+        <
+        p > Cliques { count } < /p> <
+        button onClick = {
+            () => setCount(count + 1) } >
+        Clique aqui amigo!
+        <
+        /button> <
+        /div>
+    );
+}
---
+++ src/Components/UseStateLazy.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function counter() {
+
+    return Math.random()
+
+}
+
+export default function UseStateLazy() {
+
+    // eslint-disable-next-line no-unused-vars
+    const [state, setState] = useState(counter())
+    const [num, setNum] = useState(0)
+
+    return ( <
+        div >
+        <
+        h4 > Inicialização lenta < /h4> <
+        p > Valor do state: { state } < /p> <
+            p > Valor de num: { num } < /p> <
+            button
+        onClick = {
+            () => setNum(num + 1)
+        } >
+        Clique aqui amigo!
+        <
+        /button> <
+        /div>
+    )
+}
